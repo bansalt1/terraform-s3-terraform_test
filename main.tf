7b64fa04848a2fedd68f6c89298c6421c3a21186
bansalt1 <tarun.bansal@hashicorp.com>
2024-05-20 15:44:36 +0530

    test changes

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_s3_bucket_public_access_block" "s3_bucket" {
 
   block_public_acls       = false
   block_public_policy     = false
-  ignore_public_acls      = false
+  ignore_public_acls      = true
   restrict_public_buckets = false
 }
 
